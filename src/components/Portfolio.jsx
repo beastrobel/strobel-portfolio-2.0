e0fc45ede002f10026ee43893eb8af949796a448
beastrobel <beatrizstrobel@proton.me>
2024-01-07 16:40:39 -0500

    Completed homepage

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,7 +2,7 @@ function Portfolio() {
     return(
         <section>
             <div class = "row">
-                <img src="/src/assets/TechBlog.png" class="mx-auto d-block col-12 col-md-12 col-lg-12 p-5"/>
+                <img src="/TechBlog.png" class="mx-auto d-block col-12 col-md-12 col-lg-12 p-5"/>
                 <h3 class="col-sm-12 col-lg-12">Tech Blog</h3><br/><br/>
                 <p class="col-sm-12 col-lg-12"><a href="https://tech-blog-42-62e93e33c17a.herokuapp.com/" target="blank">Deployed web application</a></p>
                 <p class="col-sm-12 col-lg-12"><a href="https://github.com/beastrobel/tech-blog" target="blank">GitHub repository</a></p> 
